abbc3387309815600ec1030d7de6c213ec210481
iceschp <warunya.ks@gmail.com>
2022-03-03 10:04:50 +0700

    Initial Commit

---
+++ fastingApp/fastingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastingAppApp.swift
+//  fastingApp
+//
+//  Created by Warunya on 3/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct fastingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
