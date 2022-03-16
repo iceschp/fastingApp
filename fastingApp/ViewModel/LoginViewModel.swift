2cb29f5fdb2defade6e6418bc7adf6e929f4e90d
jirapornNgewdang <jiraporn7494@gmail.com>
2022-03-16 15:36:14 +0700

    Login page

---
+++ fastingApp/ViewModel/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  fastingApp
+//
+//  Created by Milk Jiraporn on 16/3/2565 BE.
+//
+
+import SwiftUI
+
+struct LoginViewModel: View {
+    @Published var email:String = ""
+    @Published var password:String = ""
+}
