cb181dd36fdc8f7d49cf2f06fa68ec6ac105c5ff
iceschp <warunya.ks@gmail.com>
2022-03-18 22:09:00 +0700

    add tabview

--- fastingApp/ContentView.swift
+++ fastingApp/ContentView.swift
@@ -6,23 +6,31 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    
+    @State private var selectedIndex = 0
+    
+    init() {
+        UITabBar.appearance().unselectedItemTintColor = .orange
+        UITabBar.appearance().barTintColor = .blue
+    }
+    
     var body: some View {
-        Text("Main text")
-        TabView {
+        TabView(selection: $selectedIndex) {
             Text("Home tab")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "house.fill")
                 }
-            
+
             Text("Statistic tab")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "chart.bar.fill")
                 }
-            
+
             Text("Account tab")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
@@ -30,6 +38,21 @@ struct ContentView: View {
                 }
         }
         .accentColor(.black)
+        
+//        VStack {
+//            Text("Home")
+//
+//            ZStack {
+//                Image("picture-1")
+//                    .resizable()
+//                    .frame(width: 200, height: 200)
+//                    .position(x: 250, y: 200)
+//            }
+//
+//            HStack {
+//
+//            }
+//        }
     }
 }
 
