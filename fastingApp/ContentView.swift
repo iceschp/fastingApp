07f0df75f937207b283b0acd3947c9bf47f03d08
iceschp <warunya.ks@gmail.com>
2022-03-18 16:35:01 +0700

    add tabview

--- fastingApp/ContentView.swift
+++ fastingApp/ContentView.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Test text content")
-            .padding()
-        NavigationView {
-        LoginPage
-                .navigationBarHidden(true)
+        TabView {
+            Text("Home tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+            
+            Text("Statistic tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "chart.bar.fill")
+                }
+            
+            Text("Account tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "person.fill")
+                }
         }
+        .accentColor(.black)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
