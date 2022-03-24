89105a5f23a39ee6dddad6c0d6d9fa3a984a9025
iceschp <warunya.ks@gmail.com>
2022-03-24 11:12:24 +0700

    customize PlanHeader

--- fastingApp/MenusPlanView.swift
+++ fastingApp/MenusPlanView.swift
@@ -14,13 +14,34 @@ struct MenusPlanView: View {
 }
 
 struct PlanHeader: View {
+    
+    @State var txt = ""
+    
     var body: some View {
         VStack {
-            Text("วันนี้ทานอะไรดีคะ?")
-                .font(.system(size: 20).bold())
-                .foregroundColor(Color.pink)
+            VStack {
+                Text("วันนี้ทานอะไรดีคะ?")
+                    .font(.system(size: 20).bold())
+                    .foregroundColor(Color.pink)
+            }
+            .padding(.top, 50)
+            .padding(.bottom, 30)
             
+            HStack(spacing: 15) {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                
+                TextField("ค้นหาเมนูอาหาร . . .", text: $txt)
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal)
+            .background(Color.white)
+            .clipShape(Capsule())
+            
+            Spacer(minLength: 0)
         }
+        .padding()
+        .background(Color.green).edgesIgnoringSafeArea(.all)
     }
 }
 
