c13f3012ac203659b6ed510c7128cbed7bd8d62c
iceschp <warunya.ks@gmail.com>
2022-03-30 15:56:24 +0700

    edit struct name

--- fastingApp/timer/timeHome.swift
+++ fastingApp/timer/timeHome.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct chart: View {
+struct timeHome: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct chart_Previews: PreviewProvider {
+struct timeHome_Previews: PreviewProvider {
     static var previews: some View {
-        chart()
+        timeHome()
     }
 }
