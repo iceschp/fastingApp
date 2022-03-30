ff5949be477439d1da6399af5d828e17d73c7d90
iceschp <warunya.ks@gmail.com>
2022-03-30 15:55:24 +0700

    new timer page view

---
+++ fastingApp/timer/timeHome.swift
@@ -0,0 +1,20 @@
+//
+//  chart.swift
+//  fastingApp
+//
+//  Created by Warunya on 30/3/2565 BE.
+//
+
+import SwiftUI
+
+struct chart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct chart_Previews: PreviewProvider {
+    static var previews: some View {
+        chart()
+    }
+}
