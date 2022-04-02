e53ab65450d6bed90f68e0e58a102ff73ca1bed0
iceschp <warunya.ks@gmail.com>
2022-04-02 13:52:42 +0700

    customize timeHome page

--- fastingApp/timer/timeHome.swift
+++ fastingApp/timer/timeHome.swift
@@ -15,27 +15,21 @@ struct timeHome: View {
 
 struct timeHeader: View {
     var body: some View {
-        VStack (alignment: .leading) {
-            VStack {
-                Text("Intermitent Fasting")
-                    .font(.system(size: 25).bold())
-                    .foregroundColor(.black)
-                    .padding(.top, 20)
-                    .padding(.bottom, 20)
-                
-                HStack(spacing: 15) {
-                    Text("text")
-                }
-                .padding(.horizontal)
-            }
-            .padding(.bottom, 50)
-            .background(Color.init(uiColor: UIColor(red: 0.945, green: 0.659, blue: 0.745, alpha: 1)))
-            
-            Spacer(minLength: 0)
-            
+        ZStack {
+            Color(UIColor(red: 0.945, green: 0.659, blue: 0.745, alpha: 1))
+                .ignoresSafeArea()
         }
     }
 }
+
+struct content: View {
+    var body: some View {
+        VStack {
+            progressRing()
+        }
+    }
+}
+
 
 struct timeHome_Previews: PreviewProvider {
     static var previews: some View {
