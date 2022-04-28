c5fab58326670c6ffe0f92e49083f36fa9019a4a
iceschp <warunya.ks@gmail.com>
2022-04-28 21:10:37 +0700

    edit draft view

---
+++ fastingApp/Categories/Draft.swift
@@ -0,0 +1,20 @@
+//
+//  Draft.swift
+//  fastingApp
+//
+//  Created by Warunya on 27/4/2565 BE.
+//
+
+import SwiftUI
+
+struct Draft: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Draft_Previews: PreviewProvider {
+    static var previews: some View {
+        Draft()
+    }
+}
---
+++ fastingApp/Model/MenuBarOptions.swift
@@ -0,0 +1,20 @@
+//
+//  MenuBarOptions.swift
+//  fastingApp
+//
+//  Created by Warunya on 27/4/2565 BE.
+//
+
+import SwiftUI
+
+struct MenuBarOptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuBarOptions_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBarOptions()
+    }
+}
