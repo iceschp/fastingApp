3b387f5a95a524a1ca1a8265f14de473540bbc30
iceschp <warunya.ks@gmail.com>
2022-04-28 22:08:39 +0700

    customize CategoryHome

--- fastingApp/Categories/CategoryHome.swift
+++ fastingApp/Categories/CategoryHome.swift
@@ -15,46 +15,63 @@ struct CategoryHome: View {
 
 struct CategoryBody: View {
     @State var txt = ""
-//    @State private var selectedOption: MenuBarOptions
     
     var body: some View {
         
-        ScrollView(.vertical, showsIndicators: false, content: {
-            
-            VStack(alignment: .leading) {
-                
-                
-                VStack(spacing: 30) {
-                    Text("วันนี้ทานอะไรดีคะ?")
-                        .font(.system(size: 25).bold())
-                        .foregroundColor(Color.init(uiColor: UIColor(red: 0.945, green: 0.659, blue: 0.745, alpha: 1)))
-                        .padding(.top, 10)
+        NavigationView {
+            ScrollView {
+                LazyVStack(alignment: .leading) {
                     
-                    HStack(spacing: 15) {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
+                    VStack(spacing: 30) {
+                        Text("วันนี้ทานอะไรดีคะ?")
+                            .font(.system(size: 25).bold())
+                            .foregroundColor(Color.init(uiColor: UIColor(red: 0.945, green: 0.659, blue: 0.745, alpha: 1)))
+                            .padding(.top, 20)
                         
-                        TextField("ค้นหาเมนูอาหาร...", text: $txt)
-                            .font(.system(size: 18))
+                        HStack(spacing: 15) {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                            
+                            TextField("ค้นหาเมนูอาหาร...", text: $txt)
+                                .font(.system(size: 18))
+                        }
+                        .padding(.vertical, 12)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding(.horizontal, 15)
                     }
-                    .padding(.vertical, 12)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .clipShape(Capsule())
-                    .padding(.horizontal, 15)
-                }
-                .padding(.bottom, 40)
-                
-                Spacer(minLength: 0)
-                
-                VStack(alignment: .leading, spacing: 10) {
+                    .padding(.vertical, 40)
+                    .background(Color.init(uiColor: UIColor(red: 0.176, green: 0.412, blue: 0.282, alpha: 1)))
+                    
                     
                     VStack(alignment: .leading) {
                         Text("ประเภทอาหาร")
                             .font(.system(size: 20)).bold()
-                        CategoryRow()
                     }
                     .padding()
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top, spacing: 15) {
+                            NavigationLink(destination: AllMeal()) {
+                                ForEach(MenuBarOptions.allCases, id: \.self) { category in
+                                    VStack(spacing: 10) {
+                                        Text(category.icon)
+                                            .font(.system(size: 40))
+                                        Text(category.title)
+                                            .font(.system(size: 14).bold())
+                                            .foregroundColor(.black)
+                                        
+                                    }
+                                    .frame(width: 100, height: 120)
+                                    .background(Color.init(uiColor: category.bgColor))
+                                    .cornerRadius(10)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom)
                     
                     HStack {
                         Image("picture-2")
@@ -74,16 +91,31 @@ struct CategoryBody: View {
                     )
                     .background(Color.init(uiColor: UIColor(red: 0.851, green: 0.953, blue: 0.996, alpha: 1)))
                     .cornerRadius(30)
-                    .padding(.horizontal, 16)
+                    .padding()
+                    
+                    HStack {
+                        Text("เมนูที่น่าสนใจ 🔥")
+                            .font(.system(size: 20)).bold()
+                    }
+                    .padding()
                     
                     
+                    ForEach(meals) { meal in
+                        NavigationLink {
+                            MealRecipe(meal: meal)
+                        } label: {
+                            MealRow(meal: meal)
+                        }
+                    }
+                    .padding(.horizontal)
                     
                 }
-                .background(.white).edgesIgnoringSafeArea(.all)
-                
             }
-        })
-        .background(Color.init(uiColor: UIColor(red: 0.176, green: 0.412, blue: 0.282, alpha: 1)))
+            .frame( maxWidth: .infinity)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarHidden(true)
+        }
+        
     }
 }
 
